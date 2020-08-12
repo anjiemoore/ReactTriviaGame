3ad777aec97cc3df63550229a0eccb278eccfecf
anjiemoore <34924909+anjiemoore@users.noreply.github.com>
2020-08-12 15:34:07 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const startGame = async () => {
+
+}
+
+const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+  
+}
+
+const nextQuestion = () => {
+
+}
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>React Quiz</h1>
+      <button className="start" onClick={startGame}>Start</button>
+      <h3 className="Score">Score: </h3>
+      <h3>Loading Question</h3>
+      </div>
+  );
+}
+
+export default App;
