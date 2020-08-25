2dd429e01f13fc55b64cf9648c8bb33280ac934d
anjiemoore <34924909+anjiemoore@users.noreply.github.com>
2020-08-25 15:15:52 -0600

    getting ready for heroku

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { AnswerObject } from '../App';
 
+import { Wrapper, ButtonWrapper } from './QuestionCard.styles';
+
 type Props = {
     question: string;
     answers: string[];
@@ -11,19 +13,22 @@ type Props = {
 }
 
 const QuestionCard: React.FC<Props> = ({question, answers, callback, userAnswer, questionNum, totalQuestions}) => (
-    <div>
+    <Wrapper>
     <p className="number">Question: {questionNum} / {totalQuestions}</p>
     <p dangerouslySetInnerHTML={{__html:question}} />
     <div>
         {answers.map(answer => (
-            <div key={answer}>
+            <ButtonWrapper key={answer} 
+            correct={userAnswer?.correctAnswer === answer}
+            userClicked={userAnswer?.answer === answer}
+            >
                 <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
                     <span dangerouslySetInnerHTML={{__html: answer}}></span>
                 </button>
-            </div>
+            </ButtonWrapper>
         ))}
     </div>
-    </div>
+    </Wrapper>
 );
 
 export default QuestionCard;
